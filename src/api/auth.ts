2e4107a7a6a42a2ed4b95ee0d9a92a4c91a778c3
Daniel Teofilo <danyel.285309@gmail.com>
2023-01-10 23:49:47 -0600

    feat: fake login auth created

---
+++ src/api/auth.ts
@@ -0,0 +1,14 @@
+const fakeAuthProvider = {
+    isAuthenticated: false,
+    signIn(callback: VoidFunction) {
+      fakeAuthProvider.isAuthenticated = true;
+      setTimeout(callback, 100);
+    },
+    signOut(callback: VoidFunction) {
+      fakeAuthProvider.isAuthenticated = false;
+      setTimeout(callback, 100);
+    },
+  };
+  
+  export { fakeAuthProvider };
+  
