71db0202f2178d25b15df56c81f618aabcdd1eb4
Daniel Teofilo <danyel.285309@gmail.com>
2023-01-13 01:39:39 -0600

    feat: characterDetail component created

--- src/components/characterDetail/CharacterDetail.tsx
+++ src/components/characterDetail/CharacterDetail.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Stack,
+  useDisclosure,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { CharactersInterface } from "../../interfaces/Characters.interface";
 
-const CharacterDetail = () => {
+const CharacterDetail: React.FC<CharactersInterface> = (detail) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useEffect(() => {
+    onOpen();
+  }, [detail]);
   return (
-    <div>CharacterDetail</div>
-  )
-}
+    <div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack mt="6" spacing="3">
+              <Heading size="md">{detail.name}</Heading>
+              <Image src={detail.image} />
+              <Text as="b">estatus:</Text>
+              <Text as="i">{detail.status}</Text>
+              <Text as="b">Especie:</Text>
+              <Text as="i">{detail.species}</Text>
+              <Text as="b">Origen:</Text>
+              <Text as="i">{detail.origin.name}</Text>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
 
-export default CharacterDetail
+export default CharacterDetail;
