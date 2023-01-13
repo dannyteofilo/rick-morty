2af7e9b3f22251e5a951d7c00737a4c1f22e627d
Daniel Teofilo <danyel.285309@gmail.com>
2023-01-13 01:42:07 -0600

    feat: integration characterDetail and carousel to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,18 @@
 import { Box, Flex, Text, Image } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import CarouselSlide from "../components/Carousel/Carousel";
+import CharacterDetail from "../components/characterDetail/CharacterDetail";
 import Header from "../components/header/Header";
 import SideBar from "../components/sidebar/SideBar";
 import { generateNumberRandom } from "../helpers/common";
 import { useAppContext } from "../hooks/useAppContext";
 import { useFetchData } from "../hooks/useFetchData";
+import { CharactersInterface } from "../interfaces/Characters.interface";
 
 const Dashboard = () => {
   const { user, characters } = useAppContext();
   const { handleGetCharacters } = useFetchData();
+  const [detail, setDetail] = useState<CharactersInterface | null>(null);
 
   useEffect(() => {
     const randoms = generateNumberRandom({
@@ -19,29 +23,41 @@ const Dashboard = () => {
     handleGetCharacters(randoms);
   }, []);
 
+  const handleShowDetail = (detail: CharactersInterface) => {
+    setDetail(detail);
+  };
+
   return (
     <>
       <Header userName={user}></Header>
       <SideBar></SideBar>
       <Flex
+        width="100wh"
         height="91vh"
+        flexDirection="column"
         bgGradient="linear(137.73deg, #BBDF2B -107.85%, #00AFC8 107.28%);"
         ml={{ base: 0, md: 60 }}
+        overflow="scroll"
       >
-        <Flex flexWrap="wrap" mb="2" overflow="scroll">
+        <Flex w="80%" margin="0 auto" mt={4}>
+          <CarouselSlide></CarouselSlide>
+        </Flex>
+        <Flex flexWrap="wrap" mb="2" textAlign="center" justifyContent="center">
           {characters.map((item) => (
             <Box
               cursor="pointer"
               height={{ base: 200, md: 300, sm: 200 }}
-              width={{ base: 200, md: 300, sm: 150 }}
+              width={{ base: 140, md: 300, sm: 150 }}
               key={item.id}
-              m={8}
+              mb={8}
+              mr={{ base: 2, md: 8, sm: 4 }}
               p={2}
               borderRadius="8px"
               boxShadow="0px 35px 14px rgba(0, 0, 0, 0.01), 0px 20px 12px rgba(0, 0, 0, 0.05), 0px 9px 9px rgba(0, 0, 0, 0.09), 0px 2px 5px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)"
               backgroundColor="#1C1F24"
               display="flex"
               flexDirection="column"
+              onClick={() => handleShowDetail(item)}
             >
               <Text
                 color="white"
@@ -56,6 +72,7 @@ const Dashboard = () => {
           ))}
         </Flex>
       </Flex>
+      {detail !== null && <CharacterDetail {...detail} />}
     </>
   );
 };
